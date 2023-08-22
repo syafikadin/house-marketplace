1416456ea40f872849124285efad546fdfdd2b3a
syafikadin <syafik.adin222@gmail.com>
2023-08-22 17:17:25 +0700

    Finished 15. Get & Create Listings

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify"
 import Spinner from '../components/Spinner'
 import ListingItem from "../components/ListingItem"
 
-function Offers() {
+function Category() {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
 
@@ -21,7 +21,7 @@ function Offers() {
         // Create a query
         const q = query(
           listingsRef, 
-          where('offer', '==', true), 
+          where('type', '==', params.categoryName), 
           orderBy('timestamp', 'desc'), 
           limit(10)
         )
@@ -46,13 +46,13 @@ function Offers() {
     }
 
     fetchListings()
-  }, [])
+  }, [params.categoryName])
 
   return (
     <div className="category">
       <header>
         <p className="pageHeader">
-          Offers
+          {params.categoryName === 'rent' ? 'Place for rent' : 'Place for sale'}
         </p>
       </header>
 
@@ -69,10 +69,10 @@ function Offers() {
           </main>
           </>
         ) : (
-          <p>There are no current offers</p>
+          <p>No listings for {params.categoryName}</p>
         )}
     </div>
   )
 }
 
-export default Offers
+export default Category
